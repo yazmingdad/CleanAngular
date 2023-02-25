03d6d04d230764068642a180574b273f979e5e46
yazmingdad <yazmingdad@gmail.com>
2023-02-25 01:16:32 +0100

    styling employee-item component

--- src/app/modules/hr/employee/employee-list/employee-list.component.ts
+++ src/app/modules/hr/employee/employee-list/employee-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
 })
 export class EmployeeListComponent {
-
+  employees = [1];
 }
--- src/app/modules/hr/hr.module.ts
+++ src/app/modules/hr/hr.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HRRoutingModule } from './hr-routing.module';
 import { HomeComponent } from './home/home.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeComponent } from './employee/employee/employee.component';
 
 @NgModule({
-  declarations: [HomeComponent, EmployeeListComponent],
+  declarations: [HomeComponent, EmployeeListComponent, EmployeeComponent],
   imports: [CommonModule, HRRoutingModule],
 })
 export class HRModule {}
