521edec9c3cf272efdaf794f862a37f282a23ad3
yazmingdad <yazmingdad@gmail.com>
2023-04-25 16:13:02 +0100

    design  mission ui

--- src/app/modules/mission/mission.module.ts
+++ src/app/modules/mission/mission.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MissionRoutingModule } from './mission-routing.module';
 import { MissionListComponent } from './page/mission/mission-list/mission-list.component';
 import { HomeComponent } from './page/home/home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [MissionListComponent, HomeComponent],
-  imports: [CommonModule, MissionRoutingModule],
+  imports: [CommonModule, MissionRoutingModule, SharedModule],
 })
 export class MissionModule {}
